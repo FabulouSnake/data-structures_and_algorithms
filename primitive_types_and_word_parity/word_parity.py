748773f94ce93110d75852173cbd51e66c269503
FabulouSnake <romakcz@gmail.com>
2018-01-12 11:15:34 +0100

    added parityXORShifting function

--- primitive_types_and_word_parity/word_parity.py
+++ primitive_types_and_word_parity/word_parity.py
@@ -1,62 +1,74 @@
-# prints the 'value' number both in Decimal and Binary notation
-def printDecAndBin(value):
-	print("Decimal: {:d} \nBinary: {:b}".format(value, value))
-	print("---------------")
+			# prints the 'value' number both in Decimal and Binary notation
+			def printDecAndBin(value):
+				print("Decimal: {:d} \nBinary: {:b}".format(value, value))
+				print("---------------")
 
 
-def countOneBits(num):
-	numOfBits = 0
-	while num:
-		numOfBits += num & 1
-		num >>= 1
-	return numOfBits
+			def countOneBits(num):
+				numOfBits = 0
+				while num:
+					numOfBits += num & 1
+					num >>= 1
+				return numOfBits
 
 
-a = 50
-printDecAndBin(a)
+			a = 5
+			printDecAndBin(a)
 
-print("One Bits: " + str(countOneBits(a)))
-
-
-############################
-## WORD PARITY ALGORITHMS ##
-############################
-
-# 1. Brute-force
-def parityBruteForce(num):
-	result = 0
-	while num:
-		result ^= num & 1
-		num >>= 1
-	return result
-
-print(parityBruteForce(a))
-
-# The complexity is O(n) where n is the size of the word,
-# because we have to check every single bit
+			print("One Bits: " + str(countOneBits(a)))
 
 
+			###########################################
+			## WORD PARITY ALGORITHM IMPLEMENTATIONS ##
+			###########################################
 
-# 2. Removing the lowest 1-bit using x = x & (x -1)
-def parityRemovingLowestOneBit(num):
-	result = 0
-	while num:
-		printDecAndBin(num)
-		result ^= 1
+			# 1. Brute-force
+			def parityBruteForce(num):
+				result = 0
+				while num:
+					result ^= num & 1
+					num >>= 1
+				return result
 
-		# removes the lowest 1-bit in num
-		num &= num -1 
-	return result
+			print(parityBruteForce(a))
 
-
-print(parityUsingComplement(a))
-
-# The complexity is O(k) where k is the number of 1-bits,
-# because we need to make as many steps as there are 1-bits
-# So we can just count the number of steps and if the number
-# is odd the result is '1' and if it is even the result is '0'
+			# The complexity is O(n) where n is the size of the word,
+			# because we have to check every single bit
 
 
-def 
 
+			# 2. Removing the lowest 1-bit using x = x & (x -1)
+			def parityRemovingLowestOneBit(num):
+				result = 0
+				while num:
+					result ^= 1
+
+					# removes the lowest 1-bit in num
+					num &= num -1
+				return result
+
+
+			print(parityRemovingLowestOneBit(a))
+
+			# The complexity is O(k) where k is the number of 1-bits,
+			# because we need to make as many steps as there are 1-bits
+			# So we can just count the number of steps and if the number
+			# is odd the result is '1' and if it is even the result is '0'
+
+
+			def parityXORShifting(num):
+				num ^= num >> 32
+				num ^= num >> 16
+				num ^= num >> 8
+				num ^= num >> 4
+				num ^= num >> 2
+				num ^= num >> 1
+				return num & 1
+
+			# The complexity is O(log(n))
+			# where n is the size of the word
+			# note: x XOR 0 = x                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+			  
+
+			print(parityXORShifting(a))
 
